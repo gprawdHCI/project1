b6dbf43f9ae6e3661b2ff666fdcd0d0965357432
Gregory Prawdzik <gprawdzik@hawk.iit.edu>
2020-10-02 17:59:40 -0500

    added media/hawksm

--- assets/js/location.js
+++ assets/js/location.js
@@ -2,7 +2,7 @@ function initMap() {
     var uluru = {lat: 41.831, lng: -87.626};
     var map = new google.maps.Map(
         document.getElementById('map'), {zoom: 15, center: uluru});
-    var marker1 = new google.maps.Marker({position: uluru, map: map, icon: 'assets/media/1.png'});
+    var marker1 = new google.maps.Marker({position: uluru, map: map, icon: 'assets/media/hawksm.png'});
     var transitLayer = new google.maps.TransitLayer();
     transitLayer.setMap(map);    
   }
