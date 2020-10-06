a552799f396d7c1f8f7a54728e2988c7f082fc9f
gprawd <gprawdzik@hawk.iit.edu>
2020-10-06 17:14:08 -0500

    Update location.js

--- assets/js/location.js
+++ assets/js/location.js
@@ -1,9 +1,9 @@
 function initMap() {
-    var uluru = {lat: 41.831, lng: -87.627};
-    var uluru2 = {lat: 41.828, lng: -87.627};
-    var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 15, center: uluru});
-    var marker = new google.maps.Marker({position: uluru2, map: map, icon: 'assets/media/hawksm.png'});
-    var transitLayer = new google.maps.TransitLayer();
+  var uluru = {lat: 41.831, lng: -87.627};
+  var uluru2 = {lat: 41.828, lng: -87.627};
+  var map = new google.maps.Map(
+    document.getElementById('map'), {zoom: 15, center: uluru});
+  var marker = new google.maps.Marker({position: uluru2, map: map, icon: 'assets/media/hawksm.png'});
+  var transitLayer = new google.maps.TransitLayer();
     transitLayer.setMap(map);    
-  }
+}
